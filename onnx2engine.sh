2e12241cf0d82e7866d3c9a3c2e04b77fd9326b6
Yuzhang Gu <2238758650@qq.com>
2020-07-27 18:56:30 +0800

    add calib table cache

--- onnx2engine.sh
+++ onnx2engine.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-trtexec --onnx=pretrained/siamfc_alexnet_e50_dynamic.onnx --explicitBatch --minShapes='input':1x3x127x127 --optShapes='input':3x3x255x255 --maxShapes='input':3x3x255x255 --shapes='input':3x3x255x255 --workspace=1024 --fp16 --int8 --calib=pretrained/CalibrationTableMNISTPrediction --saveEngine=pretrained/siamfc_alexnet_e50_dynamic.engine
+trtexec --onnx=pretrained/siamfc_alexnet_e50_dynamic.onnx --explicitBatch --minShapes='input':1x3x127x127 --optShapes='input':3x3x255x255 --maxShapes='input':3x3x255x255 --shapes='input':3x3x255x255 --workspace=1024 --fp16 --int8 --calib=pretrained/CalibrationTableSiamfc --saveEngine=pretrained/siamfc_alexnet_e50_dynamic.engine
